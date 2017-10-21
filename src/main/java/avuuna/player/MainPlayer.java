2ee1fc4e586a8b41b918a06a2257cfdb827864fc
Avuunita <iguanodontecazador@gmail.com>
2017-10-20 19:54:37 -0500

    MVC

--- src/main/java/avuuna/player/MainPlayer.java
+++ src/main/java/avuuna/player/MainPlayer.java
@@ -2,12 +2,9 @@ package avuuna.player;
 
 import avuuna.player.controller.*;
 import avuuna.player.exception.*;
-import avuuna.player.gui.*;
 import avuuna.player.model.*;
 import avuuna.player.utils.*;
 
-import java.awt.*;
-import javax.swing.*;
 import javazoom.jl.player.basic.*;
 
 /**
@@ -25,26 +22,9 @@ public class MainPlayer {
 			
 			PlayerController controller = new PlayerController(basicPlayer);
 			player.addObserver(controller);
-//			PlayerPanel playerPanel = new PlayerPanel(player);
-//			PlaylistPanel playlistPanel = new PlaylistPanel(player);
-//
-//			PlayerFrame playerFrame = new PlayerFrame(player, playerPanel, playlistPanel);
-//			drawFrame(playerFrame);
-//			player.addObserver(playerFrame.getPlayerPanel());
-//			player.addObserver(playerFrame.getPlaylistPanel());
 		} catch (LookAndFeelException ex) {
 			Utils.display(ex.getClass().getName() + ": " + ex.getMessage());
 			Utils.log(MainPlayer.class.getName(), ex);
 		}
-	}
-
-	private static void drawFrame(PlayerFrame frame) {
-		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, frame.getPlayerPanel(), frame.getPlaylistPanel());
-		splitPane.setEnabled(false);
-		frame.add(splitPane, BorderLayout.CENTER);
-		frame.pack();
-		frame.setVisible(true);
-		frame.setResizable(false);
-		frame.setLocationRelativeTo(null);
 	}
 }
